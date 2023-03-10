247d7465373f9873136ec70fe6453311768419e0
Piskun_Ivan <tw1st3r111@mail.ru>
2023-03-11 00:51:54 +0300

    task2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,8 +4,17 @@ menuLink.forEach((element) => {
         const menuItem = element.closest('.menu__item');
         const subMenu = menuItem.querySelector('.menu_sub');
         if (subMenu) {
-            subMenu.classList.add('menu_active');
+            const subMenuCheck = subMenu.classList.contains('menu_active');
+            const arraySubMenu = Array.from(document.querySelectorAll('ul.menu_active'));
+            arraySubMenu.forEach((elem) => {
+                elem.classList.remove('menu_active');
+            });
+            if (!subMenuCheck) {
+                subMenu.classList.add('menu_active');
+            }
         }
         return false
     }
 });
+
+
