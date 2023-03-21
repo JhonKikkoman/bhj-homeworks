6cae68aebcd01552daf062123668ae2c1d382560
Piskun_Ivan <tw1st3r111@mail.ru>
2023-03-22 00:54:50 +0300

    task3

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,31 +4,49 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.countDownTimer = container.querySelector('.countdown');
+    this.setId = null;
 
     this.reset();
 
     this.registerEvents();
   }
 
+  countDown(wordLen) {
+    this.countDownTimer.textContent = wordLen;
+    this.setId = setInterval(() => {
+      const num = Number(wordLen);
+      if (this.setId != null) {
+        clearInterval(this.setId);
+        this.setId = null;
+      }
+      num - 1;
+      this.countDownTimer.textContent = num;
+      if (num === 0) {
+        fail();
+        clearInterval(this.setId);
+      }
+    }, 1000);
+  }
+
   reset() {
+    this.countDown();
     this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (e) => {
+      if (e.repeat) {
+        return
+      }
+      e.key.toLocaleLowerCase() === this.currentSymbol.textContent ? this.success() : this.fail();
+    });
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    if (this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
@@ -54,24 +72,24 @@ class Game {
 
   setNewWord() {
     const word = this.getWord();
-
+    this.countDown(word.length)
     this.renderWord(word);
   }
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -81,7 +99,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
