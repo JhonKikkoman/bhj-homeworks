c7fb2a3a700043f76431675e0712829c6c0f4def
Piskun_Ivan <tw1st3r111@mail.ru>
2023-04-18 00:15:20 +0300

    task7

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,5 +1,10 @@
 const loader = document.querySelector('img.loader_active');
 const items = document.getElementById('items');
+const storage = window.localStorage;
+
+loader.classList.remove('loader_active');
+let lastValue = JSON.parse(storage.getItem('lastValue'));
+exchangeRate(lastValue);
 
 let xhr = new XMLHttpRequest();
 
@@ -7,14 +12,19 @@ xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-course
 xhr.responseType = "json";
 xhr.send();
 xhr.onload = function () {
-    if (xhr.readyState === xhr.DONE) {
-        loader.classList.remove('loader_active');
-        for (let key in xhr.response.response.Valute) {
-            items.insertAdjacentHTML('afterbegin',
-                `<div class="item"><div class="item__code">${xhr.response.response.Valute[key].CharCode}</div>
-        <div class="item__value">${xhr.response.response.Valute[key].Value}</div>
-        <div class="item__currency">руб.</div></div>`);
+    if (xhr.status === 200) {
+        if (xhr.readyState === xhr.DONE) {
+            storage.setItem('lastValue', JSON.stringify(xhr.response.response.Valute));
+            exchangeRate(xhr.response.response.Valute);
         }
     }
 };
 
+function exchangeRate(obj) {
+    for (let key in obj) {
+        items.insertAdjacentHTML('afterbegin',
+            `<div class="item"><div class="item__code">${obj[key].CharCode}</div>
+<div class="item__value">${obj[key].Value}</div>
+<div class="item__currency">руб.</div></div>`);
+    }
+}
