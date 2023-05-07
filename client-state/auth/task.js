5bf3dddac6bba528b08728d93141182d17526b58
Piskun_Ivan <tw1st3r111@mail.ru>
2023-05-07 18:20:41 +0300

    task8

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -11,31 +11,39 @@ btnSubmit.addEventListener('click', (e) => {
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     xhr.responseType = 'json';
     xhr.send(formData);
-    xhr.addEventListener('load', () => {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.response.success) {
-                localStorage.setItem('userId', xhr.response.user_id);
-                signinBlock.classList.remove('signin_active');
-                welcomePopup.classList.add('welcome_active');
-                let currentUser = localStorage.getItem('userId');
-                userId.textContent = currentUser;
-            } else {
-                alert('Неверный логин/Пароль');
-            }
-        }
-    });
+    xhr.addEventListener('load', () => loginResponse(xhr.response));
 });
+
+function loginResponse(serverResponse) {
+    if (serverResponse.success) {
+        localStorage.setItem('userId', serverResponse.user_id);
+        changeContent(localStorage.getItem('userId'));
+    } else {
+        alert('Неверный логин/Пароль');
+    }
+}
+
+function changeContent(currentUser) {
+    userId.textContent = currentUser;
+    signinBlock.classList.remove('signin_active');
+    welcomePopup.classList.add('welcome_active');
+}
+
+if (localStorage.getItem('userId') != undefined) {
+    changeContent(localStorage.getItem('userId'));
+}
 
 let btnLogout = document.createElement('button');
 btnLogout.innerHTML = 'X';
 btnLogout.style = "border-radius: 50%; float: right";
 welcomePopup.prepend(btnLogout);
+btnLogout.addEventListener('click', clickLogout);
 
-btnLogout.addEventListener('click', () => {
+function clickLogout() {
     localStorage.removeItem('userId');
     formUser.reset();
     welcomePopup.classList.remove('welcome_active');
     signinBlock.classList.add('signin_active');
-});
+}
 
 
